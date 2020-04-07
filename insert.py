3e2b4547a6b5cc0a8589c029bc5a220ff46f742e
Kushwanth Gondi <36468390+KushwanthG@users.noreply.github.com>
2020-04-07 16:51:58 +0530

    Add files via upload

---
+++ insert.py
@@ -0,0 +1,9 @@
+print('this is a {}'.format('string'))
+print('the {} {} {}'.format("tiger",'lion',"chetha"))
+print('the {2} {0} {1}'.format("tiger",'lion',"chetha"))
+print('the {1} {1} {1}'.format("tiger",'lion',"chetha"))
+print('the {c} {a} {b}'.format(a="tiger",b='lion',c="chetha"))
+result=3762.2952863475
+print("the result was {r:2.8f}".format(r=result)) 
+name='joe'
+print(f'the name is {name}')
